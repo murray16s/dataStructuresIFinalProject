df5d3048f35a32aaf2f515bf372912286ab56067
murray16s <murray16s@ncssm.edu>
2015-10-29 14:13:09 +0000

    remove=ing test scripts

--- test.pyx
+++
@@ -1,9 +0,0 @@
-from libc.stdio cimport *
-def main():
-    cdef char* test = "Hello World!"
-    pystr = test
-    pystr += "\nIt's nice to see you!\n\n\n"
-    test = pystr
-    printf(test)
-    printf("We have to go back...\n\n\nTo the future!!!\n")
-main()
