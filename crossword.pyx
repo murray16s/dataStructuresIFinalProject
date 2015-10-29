c2fcafe3abd3af682da9560b024d3524cba873af
murray16s <murray16s@ncssm.edu>
2015-10-29 19:43:14 +0000

    Just laying out some ideas

---
+++ crossword.pyx
@@ -0,0 +1,13 @@
+from libc.stdio cimport *
+from math import *
+cdef int main():
+    
+    return 0
+def makeBoard():
+    char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',' k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    fillIns = ['_', '#']
+    # We want to weight the fillIns more, so that they occur more often than the chars.
+    # cdef struct for the board
+    # use random Integer to determine whether each space is char or fillIn (10% or 20% char?).
+    # Randomly assign thw fillIns and the chars
+    pass
